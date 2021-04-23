f790a298601f87c734dd192000bc345c535e1f7a
Johnny Xu <jxu@pernix-solutions.com>
2021-04-23 11:16:48 -0600

    Move home screen out

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import moment from 'moment';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Text>{moment().format('LLLL')}</Text>
-    </View>
-  );
-}
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
